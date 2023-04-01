be0b0d25919325c5e2f75ab46ca23aabe9df5999
maxime gaudron <maxime.kylox.gaudron@gmail.com>
2023-04-01 11:49:05 +0200

    update to python3
    Signed-off-by: maxime gaudron <maxime.kylox.gaudron@gmail.com>

--- src/car.py
+++ src/car.py
@@ -4,12 +4,12 @@ UDP_IP = "192.168.24.1"
 UDP_PORT = 4210
 MESSAGE = "Hi, can you listen to this?"
 
-print "UDP target IP:", UDP_IP
-print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
+print("UDP target IP:", UDP_IP)
+print("UDP target port:", UDP_PORT)
+print("message:", MESSAGE)
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.sendto(MESSAGE.encode('ascii'), (UDP_IP, UDP_PORT))
 
-print "sended"
+print("sended")
